1ce9369c7f4cccc33863cd2634b66fb58772989c
paiain <86291726+paiain@users.noreply.github.com>
2021-09-12 02:57:02 +0700

    modify

--- web/local_settings.py
+++ web/local_settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'newweb',
+        'NAME': 'new',
         'USER': 'paisin',
         'PASSWORD': '12345',
         'HOST': 'localhost',
